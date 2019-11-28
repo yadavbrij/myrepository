877bfa7bbdb4b678a500684a1c7653fbd92f27a1
yadavbrij <58269565+yadavbrij@users.noreply.github.com>
2019-11-28 12:41:11 +0530

    Array-method
    Using Array function

---
+++ CreateArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Summary description for CreateArray
+    /// </summary>
+    [TestClass]
+    public class CreateArray
+    {
+        public  void VerfiyCreateArray()
+        {
+            int[] arr = new int[5];
+            arr[0] = 10;
+            arr[1] = 20;
+            arr[2] = 30;
+            arr[3] = 40;
+            arr[4] = 50;
+
+            for(int i=0; i<arr.Length;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CreateArray ca = new CreateArray();
+            ca.VerfiyCreateArray();
+
+        }
+    }
+
+}
