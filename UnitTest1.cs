1b1dc72c05a7b7170f7135752b1029e94913f269
yadavbrij <58269565+yadavbrij@users.noreply.github.com>
2019-11-28 13:11:01 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        public void  Verfiy_UnitTest1()
+        {
+            var names = new List<String>();
+            names.Add("java");
+            names.Add("testing");
+            names.Add("C#");
+
+            foreach(var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+      
+        [TestMethod]
+        public void TestMethod1()
+        {
+            UnitTest1 ut = new UnitTest1();
+            ut.Verfiy_UnitTest1();
+        }
+    }
+}
